c965531309b19ed7ceb24b677994c9857670ab6a
P-S-Y-S-U <mohan.gaming78@gmail.com>
2025-05-22 17:11:48 +0530

    drm device info sample updated

--- drm_device_info.cpp
+++ drm_device_info.cpp
@@ -12,6 +12,50 @@
 #include <cstring>
 
 
+inline const char* getDrmConnectionStr( const uint32_t& connection )
+{
+    switch (connection)
+    {
+    case DRM_MODE_CONNECTED:
+        return "CONNECTED";
+    case DRM_MODE_DISCONNECTED:
+        return "DISCONNECTED";
+    case DRM_MODE_UNKNOWNCONNECTION:
+        return "UNKOWNCONNECTION";
+    default:
+        return "UNKNOWNENUM";
+    }
+}
+
+inline const char* getDrmConnectorTypeStr( const uint32_t& connector_type )
+{
+    switch (connector_type)
+    {
+        case DRM_MODE_CONNECTOR_Unknown: return "Unknown";
+        case DRM_MODE_CONNECTOR_VGA: return "VGA";
+        case DRM_MODE_CONNECTOR_DVII: return "DVII";
+        case DRM_MODE_CONNECTOR_DVID: return "DVID";
+        case DRM_MODE_CONNECTOR_DVIA: return "DVIA";
+        case DRM_MODE_CONNECTOR_Composite: return "Composite";
+        case DRM_MODE_CONNECTOR_SVIDEO: return "SVIDEO";
+        case DRM_MODE_CONNECTOR_LVDS: return "LVDS";
+        case DRM_MODE_CONNECTOR_Component: return "Component";
+        case DRM_MODE_CONNECTOR_9PinDIN: return "9PinDIN";
+        case DRM_MODE_CONNECTOR_DisplayPort: return "DisplayPort";
+        case DRM_MODE_CONNECTOR_HDMIA: return "HDMI";
+        case DRM_MODE_CONNECTOR_HDMIB: return "HDMIB";
+        case DRM_MODE_CONNECTOR_TV: return "TV";
+        case DRM_MODE_CONNECTOR_eDP: return "eDP";
+        case DRM_MODE_CONNECTOR_VIRTUAL: return "VIRTUAL";
+        case DRM_MODE_CONNECTOR_DSI: return "DSI";
+        case DRM_MODE_CONNECTOR_DPI: return "DPI";
+        case DRM_MODE_CONNECTOR_WRITEBACK: return "WRITEBACK";
+        case DRM_MODE_CONNECTOR_SPI: return "SPI";
+        case DRM_MODE_CONNECTOR_USB: return "USB";
+        default: return "UNKNOWNENUM";
+    }
+}
+
 int main( int argc, const char* argv[] )
 {
 
@@ -30,6 +74,35 @@ int main( int argc, const char* argv[] )
     drmModeResPtr resources = drmModeGetResources(drm_fd);
 
     std::cout << "drm resouces:\n fbs: " << resources->count_fbs << "\n crtcs: " << resources->count_crtcs << "\n connectors: " << resources->count_connectors << "\n encoders: " << resources->count_encoders << "\n min_extents: " << resources->min_width << "x" << resources->min_height << "\n max_extents: " << resources->max_width << "x" << resources->max_height << "\n";
+
+    for( int i = 0; i < resources->count_connectors; i++ )
+    {
+        drmModeConnectorPtr drm_connector = drmModeGetConnector( drm_fd, resources->connectors[i] );
+        int result;
+        
+        if(!drm_connector)
+            continue;
+        
+        std::cout << "Connector: " <<  \
+                                        getDrmConnectionStr(drm_connector->connection) << "(" << drm_connector->connector_id << ") " << \
+                                        getDrmConnectorTypeStr(drm_connector->connector_type) << "(" << drm_connector->connector_type_id << ") " << \
+                                        "number of modes: " << drm_connector->count_modes << "\n";
+
+
+        if( drm_connector->count_modes == 0 ) {
+            std::cout << " No Valid Modes for connector!! \n\n";
+            continue;
+        }
+
+        std::cout << "mode info:\n";
+        for(int j = 0; j < drm_connector->count_modes; j++)
+        {
+            drmModeModeInfo modeInfo = drm_connector->modes[j];
+            std::cout << "connector_id: " <<  drm_connector->connector_id << " " << modeInfo.hdisplay << "x" << modeInfo.vdisplay << "(" << modeInfo.vrefresh << ")\n";
+        }
+
+        std::cout << "\n\n";
+    }
 
     return 0;
 }
